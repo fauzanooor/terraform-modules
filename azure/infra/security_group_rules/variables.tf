d0daa7d57350fd553c59f34e2621ad00effe9cde
fauzan <fauzanooor@gmail.com>
2020-11-09 10:21:51 +0700

    FIX: adding null_resource for rule sg depends_on

--- azure/infra/security_group_rules/variables.tf
+++ azure/infra/security_group_rules/variables.tf
@@ -22,6 +22,5 @@ variable "sg-rule-destination_address_prefix" {
 variable "resource_group_name" {}
 variable "sg-name" {}
 variable "sg_depends_on" {
-    type = any
-    default = null
+    default = [""]
 }
